345458a2cd2e03955217306f75a8a16efc523319
Frank Yang <puilp0502@gmail.com>
2018-05-03 17:20:52 +0900

    Create ranking app's model

--- ranking/models.py
+++ ranking/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=80, blank=False)
+
+    def __str__(self):
+        return '<Publisher %s>' % self.name
+
+
+class Paper(models.Model):
+    title = models.CharField(max_length=140, blank=False)
+    author = models.CharField(max_length=40, blank=False)
+    year = models.IntegerField(null=False)
+    publisher = models.ForeignKey(Publisher, on_delete=models.PROTECT)
+    score = models.FloatField(null=False)
+
+    def short_author(self):
+        return self.author.split(' ')[0] + ' et al.'
+
+    def __str__(self):
+        return '"%s" by %s' % (self.title, self.short_author())
