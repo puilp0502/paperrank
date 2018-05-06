2739c4257f21c2d7f87676291ad36c5d5549e63d
Frank Yang <puilp0502@gmail.com>
2018-05-07 02:45:21 +0900

    Add abstract and slug field to Paper model

--- ranking/models.py
+++ ranking/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils.text import slugify
 
 
 class Publisher(models.Model):
@@ -10,9 +13,11 @@ class Publisher(models.Model):
 
 class Paper(models.Model):
     title = models.CharField(max_length=140, blank=False)
+    slug = models.SlugField(max_length=50, blank=True)  # auto-generated from title
     author = models.CharField(max_length=40, blank=False)
     year = models.IntegerField(null=False)
     publisher = models.ForeignKey(Publisher, on_delete=models.PROTECT)
+    abstract = models.TextField(blank=True)
     score = models.FloatField(null=False)
 
     def short_author(self):
@@ -20,3 +25,10 @@ class Paper(models.Model):
 
     def __str__(self):
         return '"%s" by %s' % (self.title, self.short_author())
+
+
+@receiver(pre_save, sender=Paper)
+def autofill_slug(sender, **kwargs):
+    instance = kwargs['instance']
+    if not instance.slug:
+        instance.slug = slugify(instance.title)[:50]
