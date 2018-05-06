700c41cb5b3957085327ab9c31c63f01aa567020
Frank Yang <puilp0502@gmail.com>
2018-05-07 02:46:17 +0900

    Add URL namespace

--- paperscore/urls.py
+++ paperscore/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ranking.urls')),
+    path('', include('ranking.urls', namespace='ranking')),
 ]
--- ranking/urls.py
+++ ranking/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import paper_rank
 
 
+app_name = 'ranking'
 urlpatterns = [
-    path('rank/', paper_rank),
+    path('rank/', paper_rank, name='rank'),
 ]
